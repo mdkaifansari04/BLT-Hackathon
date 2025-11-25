b18df0ab2ea320c5e162f0b020aca55f61d14729
Md Kaif Ansari <amdkaif843@gmail.com>
2025-11-26 01:59:32 +0530

    refactor: updated the module name

--- plugin/django-hackathon/blt_hackathon/apps.py
+++ plugin/django-hackathon/blt_hackathon/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class HackathonConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "hackathon"
+    name = "blt_hackathon"
     verbose_name = "Hackathon Management"
     
     def ready(self):
